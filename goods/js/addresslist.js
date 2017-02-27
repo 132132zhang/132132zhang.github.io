9f1ec432de93edb4f1ff1939c8a324ef37a8eeb2
zhangliya <13213276910@163.com>
2017-02-27 14:46:12 +0800

    新增加项目分享页面和商品页面

---
+++ goods/js/addresslist.js
@@ -0,0 +1,16 @@
+/**
+ * Created by mac on 2017/2/24.
+ */
+$(function(){
+	$(".add-list-ul li").click(function(){
+		$(".add-list-ul li").removeClass("checked");
+		$(this).addClass("checked");
+		location.href="gotobuy.html"
+	})
+
+
+
+
+
+
+});
